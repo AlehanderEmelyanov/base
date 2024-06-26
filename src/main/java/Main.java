117d248f681e0627c278e0409387b551048bb3a6
AlehanderEmelyanov <sashaem85@gmail.com>
2024-06-26 16:12:11 +0300

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,5 @@ public class Main {
     public static void main(String[] args){
 
 
-        Move move = new Move();
-
-        move.iditems=1;
-        move.idperson=3;
-
-        CRUDMove.create(move);
     }
 }
