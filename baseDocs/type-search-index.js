3e2515151f3bca288133e4383043deab701b8c82
AlehanderEmelyanov <sashaem85@gmail.com>
2024-06-26 22:54:08 +0300

    final(add instruction(docs))

---
+++ baseDocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CRUD"},{"p":"<Unnamed>","l":"CRUDMove"},{"p":"<Unnamed>","l":"CRUDU"},{"p":"<Unnamed>","l":"DB"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Move"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"PersonData"},{"p":"<Unnamed>","l":"sqlComand"}];updateSearchResults();
